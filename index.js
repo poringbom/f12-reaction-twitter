899967e7206d1e3d84d1e5bc80d25bce994b7822
poringbom <bom_kritsada@hotmail.com>
2023-05-04 14:52:25 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+let position = 0;
+
+setInterval(()=> {
+    window.scrollTo(0, position+=1000)    
+    reaction();
+}, 1000)
+
+
+
+function reaction() {
+    var filter   = Array.prototype.filter
+    const article = document.querySelectorAll("article")
+    const filtered = filter.call( article , function( node ) {
+        return !!node.querySelectorAll('a[href="/optimismFND"]').length;
+    });
+    filtered.forEach(post => {
+      rewit(post);
+      like(post);
+    });
+}
+
+
+function rewit(post){
+    const btn = post.querySelector('div[data-testid="retweet"]');
+    if (btn) {
+        btn.click();
+        setTimeout(()=> {
+            const cfRewit = document.querySelector('div[data-testid="retweetConfirm"]');
+            cfRewit.click();
+         }, 300);
+    }
+}
+
+
+function like(post) {
+    const btn = post.querySelector('div[data-testid="like"]');
+     if (btn) {
+        btn.click();
+     }
+}
